30032db06d56409adaff20d14f82a89da4cb2f00
Melandri <56926590+sSelmann@users.noreply.github.com>
2024-10-22 05:38:24 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -28,9 +28,28 @@ const chain = (urlParams.get("chain") && urlParams.get("chain")?.startsWith("{")
 
 const clientId = urlParams.get("clientId") || clientIdConst || "";
 
+const customChain = {
+  // Required information for connecting to the network
+  chainId: 33139, // Chain ID of the network
+  rpc: ["https://33139.rpc.thirdweb.com/35948768453756f1cf091e34b46d265a"], // Array of RPC URLs to use
+ 
+  // Information for adding the network to your wallet (how it will appear for first time users) === \\
+  // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
+  nativeCurrency: {
+    decimals: 18,
+    name: "APE",
+    symbol: "APE",
+  },
+  shortName: "ape", // Display value shown in the wallet UI
+  slug: "APE", // Display value shown in the wallet UI
+  testnet: false, // Boolean indicating whether the chain is a testnet or mainnet
+  chain: "Ape Chain", // Name of the network
+  name: "Ape Chain Mainnet", // Name of the network
+};
+
 root.render(
   <React.StrictMode>
-    <ThirdwebProvider activeChain={chain} sdkOptions={sdkOptions} clientId={clientId}>
+    <ThirdwebProvider activeChain={customChain} sdkOptions={sdkOptions} clientId={clientId}>
       <Toaster />
       <App />
     </ThirdwebProvider>
